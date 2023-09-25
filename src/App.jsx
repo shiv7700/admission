b6adfe182b5430ea2aec3b7bfc4b0f62049c71ff
ShivanG <shivangramola007@gmail.com>
2023-09-25 15:42:29 +0530

    remove some files

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ import Download from './components/Download'
 import b1 from './assets/know-clg.png'
 import b2 from './assets/know-gn.png'
 import Skill from './components/Skill'
-import Header from './components/Header'
 import Navbar from './components/Navbar'
 
 const App = () => {
